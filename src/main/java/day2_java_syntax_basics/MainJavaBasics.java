f1d882fd7781f3af5e923beafea45c09002d096d
SpectreSpammer <rnavarra.business@gmail.com>
2024-10-29 19:27:05 +0800

    Oct 29- Day 1 Accomplishments → Day 1: Setting up environment and Day 2: Java Syntax Basics

---
+++ src/main/java/day2_java_syntax_basics/MainJavaBasics.java
@@ -0,0 +1,18 @@
+package day2_java_syntax_basics;
+
+import java.util.Scanner;
+
+/*
+        this is only for documentation purposes
+*/
+public class MainJavaBasics {
+    public static void main(String[] args) {
+
+        //this is an one line comment
+        System.out.println();
+
+    }
+
+    //say hello
+
+}
